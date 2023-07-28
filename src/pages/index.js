ff4a02ba640a6fa18d4458e87594637736021452
lucapcf <lucapcf@gmail.com>
2023-07-28 16:35:42 -0300

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,18 @@ import styles from '../styles/Home.module.css'
 import { handleJSONfiles } from '@/utils/functions/jsonHandler'
 
 
-export default function Home({membros}) {
-  console.log(membros)
+export default function Home({exemplos}) {
+  console.log(exemplos)
   return (
     <div className={styles['container']}>
       <Head><title>Template Next Netlify</title></Head>
       <h1 className={styles['header']}>Este é um template de Next - Netlify</h1>
       <p className={styles['subtitle']}>Abra /admin para ver as opções</p>
-      <ul className={styles['membro-list']}>
-        {membros && membros.map((membro,i) => (
-          <Link className={styles["link"]} key={i} href={`/membros/${membro.fileName}`}>
+      <ul className={styles['exemplo-list']}>
+        {exemplos && exemplos.map((exemplo,i) => (
+          <Link className={styles["link"]} key={i} href={`/exemplos/${exemplo.fileName}`}>
             <div className={styles["card"]}>
-                <span >{membro.nome}</span>
+                <span >{exemplo.titulo}</span>
             </div>
           </Link>
         ))}
@@ -25,8 +25,8 @@ export default function Home({membros}) {
 }
 
 export async function getStaticProps(){
-  const membros = handleJSONfiles("./content/membros");
+  const exemplos = handleJSONfiles("./content/exemplos");
   return {
-    props: { membros },
+    props: { exemplos },
   };
 }
