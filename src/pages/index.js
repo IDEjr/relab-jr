06b0fa9ca0d58dc8683eed381c4ef97bd3bfbf6e
lucapcf <lucapcf@gmail.com>
2023-07-29 13:02:27 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 import { handleJSONfiles } from '@/utils/functions/jsonHandler'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
+import Image from 'next/image'
 
 
 export default function Home({membros}) {
@@ -13,17 +14,24 @@ export default function Home({membros}) {
       <div className={styles['tudo']}>
         <Navbar />
         <div className={styles['container']}>
-          <Head><title>Template Next Netlify</title></Head>
-          <h1 className={styles['header']}>Este é um template de Next - Netlify</h1>
-          <p className={styles['subtitle']}>Abra /admin para ver as opções</p>
+          <Head><title>Exemplo Next Netlify</title></Head>
+          <h1 className={styles['header']}>Exemplo</h1>
+          <p className={styles['subtitle']}>Abra /admin para acessar configurações netlify</p>
           <ul className={styles['membro-list']}>
             {membros && membros.map((membro,i) => (
               <a target="_blank" href={`${membro.linkedin}`} className={styles["link"]} key={i}>
+                <div>
+                  <Image
+                    src={`${membro.imagem}`}
+                    width={500}
+                    height={500}
+                    alt="Membro"
+                  />
+                </div>
                 <div className={styles["card"]}>
-                    <span >{membro.texto}</span>
+                  <span >{membro.texto}</span>
                 </div>
               </a>
-              
             ))}
           </ul>
         </div>
