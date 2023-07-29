0800b63d3f65a21bef72ecaea9939020548c73d9
lucapcf <lucapcf@gmail.com>
2023-07-29 20:35:58 -0300

    .

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,16 +2,18 @@ import Link from 'next/link'
 import styles from '../styles/footer.module.css'
 
 export default () => (
-    <div className='footer'>
-        <br></br>
-        <br></br>
-        <br></br>
-        <h1 className={styles["box"]}>Footer</h1>
-        <h2>
-            <Link href="/">Back to home</Link>
-        </h2>
-        <br></br>
-        <br></br>
-        <br></br>
-    </div>
+    <>
+        <div className='footer'>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h1 className={styles["box"]}>Footer</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+            <br></br>
+            <br></br>
+            <br></br>
+        </div>
+    </>
 );
--- src/pages/quemSomos.js
+++ src/pages/quemSomos.js
@@ -21,8 +21,8 @@ export default function quemSomos({membros}) {
             <div>
               <Image
                 src={`${membro.imagem}`}
-                width={500}
-                height={500}
+                width={400}
+                height={400}
                 alt="Membro"
               />
             </div>
