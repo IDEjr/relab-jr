7774458a14e26c5a02c03f0c2c9ead9263dd5300
lucapcf <lucapcf@gmail.com>
2023-08-08 22:52:26 -0300

    v1.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,27 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/home.module.css'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 import ImagemComBotao from '../components/imagemComBotao'
-import imagem_resultados from '../images/imagem_resultados.png'
-
+import imagemResultados from '../images/imagemResultados.png'
+import imagemMembros from '../images/imagemMembros.png'
 
 export default function Home() {
-  const featuredProps = {
-    imagem: imagem_resultados,
+  const resultados = {
+    imagem: imagemResultados,
     titulo: 'MELHORE SEUS RESULTADOS',
     texto: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget libero viverra velit pellentesque hendrerit. Donec interdum dolor nisl, at rhoncus dolor consectetur euismod. Aliquam scelerisque finibus egestas.',
-    texto_botao: 'Entre em contato'
+    textoBotao: 'Entre em contato'
   };
+
+  const membros = {
+    imagem: imagemMembros,
+    titulo: 'MEMBROS',
+    texto: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget libero viverra velit pellentesque hendrerit. Donec interdum dolor nisl, at rhoncus dolor consectetur euismod. Aliquam scelerisque finibus egestas.',
+    textoBotao: 'Sejo membro'
+  };
+
   return (
     <>
       <div className={styles['tudo']}>
@@ -23,7 +31,8 @@ export default function Home() {
           <h1 className={styles['header']}>Exemplo</h1>
           <p className={styles['subtitle']}>Abra /admin para acessar configurações netlify</p>
         </div>
-        <ImagemComBotao {...featuredProps} />
+        <ImagemComBotao {...resultados} />
+        <ImagemComBotao {...membros} />        
         <Footer />
       </div>
     </>
