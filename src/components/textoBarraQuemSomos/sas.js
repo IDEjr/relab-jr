8665d8e007186c7f13c7aad9d59beb37da573146
lucapcf <lucapcf@gmail.com>
2023-08-10 19:15:47 -0300

    adicionando textoBarraQuemSomos

---
+++ src/components/textoBarraQuemSomos/sas.js
@@ -0,0 +1,27 @@
+import styles from './as.css'
+import Link from 'next/link'
+
+
+export default () => (
+    <>
+        <h1>Quem Somos</h1>
+        <h2>
+            <Link href='/'>Back to home</Link>
+        </h2>
+        <div className={styles['toptext']}>
+            
+            <p className={styles['text-content']} >
+                Texto explicando um pouco da história da Relab jr e seus valores. 
+                Contando o como começou, com que intuito, quais suas maiores causas.
+            </p>  
+        </div>
+        
+        <div className={styles['bottomtext']}>
+            
+            <p className={styles['text-content']}>
+            E também falando um pouco melhor sobre as metas gerais e norteadoras da empresa no futuro.
+            Texto meramente ilustrativo.
+            </p>
+        </div>
+    </>
+);
