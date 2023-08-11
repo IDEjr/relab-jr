da3e9f966b621d8cdcd22fe27c0dbed50d67dd3a
Nickolas Xisto Machado <cabocanick@gmail.com>
2023-08-11 17:59:15 -0300

    iniciando responsividade e adição do ícone
    Responsividade realizada para todas as resoluções de pc e iniciando as resoluções do mobile

---
+++ src/components/textoBarraQuemSomos/index.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import styles from './textoBarraQuemSomos.module.css'
+import Image from 'next/image'
+import image1 from '../../../public/uploads/image/QuemSomos/iconelampada.png'
+
+export default function textoBarraQuemSomos() {
+    return(
+        <>
+            <h1>Quem Somos</h1>
+            <h2><Link href='/'>Back to home</Link></h2>
+
+            <div className={styles['toptext']}>
+                <p className={styles['text-content']} >
+                    Texto explicando um pouco da história da Relab jr e seus valores. 
+                    Contando o como começou, com que intuito, quais suas maiores causas.
+                </p>  
+            </div>
+            <div className={styles['main']}>
+            <Image src={image1} className={styles['lampada']}/>
+            <div className={styles['bottomtext']}>
+                <p className={styles['text-content']}>
+                    E também falando um pouco melhor sobre as metas gerais e norteadoras da empresa no futuro.
+                    Texto meramente ilustrativo.
+                </p>
+            </div>
+            </div>
+        </>
+    )
+}
