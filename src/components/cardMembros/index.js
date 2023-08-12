ab6511c664d5e5a0654d746946938859729a6a32
lucapcf <lucapcf@gmail.com>
2023-08-11 21:08:50 -0300

    .

--- src/components/cardMembros/index.js
+++ src/components/cardMembros/index.js
@@ -2,13 +2,24 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from './cardMembros.module.css'
 
-export default function cardMembros({ nome, foto, posição, linkedin }) {
+export default function cardMembros({ nome, foto, posicao, linkedin }) {
   return (
     <>
       <div className={styles['container']}>
         <div className={styles['box']}>
-          <h2 className={styles['titulo']}>{titulo}</h2>
-          <p className={styles['texto']}>{texto}</p>
+          <h2 className={styles['titulo']}>{nome}</h2>
+          <p className={styles['texto']}>{posicao}</p>
+          <Link>{linkedin}</Link>
+          <Image
+            src={foto}
+            // fill
+            style={{objectFit: 'cover'}}
+            // style={{background-size: 'cover'}}
+            width={500}
+            height={300}
+            alt='Foto Membro'
+            // className={styles['background']}
+            />
         </div>
       </div>
     </>
