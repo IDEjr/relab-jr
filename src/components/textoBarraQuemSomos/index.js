5b290724538fe2c2b15549d018144c787f97574a
Nickolas Xisto Machado <cabocanick@gmail.com>
2023-08-17 10:25:38 -0300

    refatorando css

--- src/components/textoBarraQuemSomos/index.js
+++ src/components/textoBarraQuemSomos/index.js
@@ -9,14 +9,14 @@ import image3 from '../../../public/uploads/image/QuemSomos/sorrisoRelab.png'
 export default function textoBarraQuemSomos() {
     return(
         <>
-        <div className={styles['jose']}>  
+        <div className={styles['container']}>  
           <div className={styles['toptext']}>
                 <p className={styles['text-content']} >
                     Texto explicando um pouco da história da Relab jr e seus valores. 
                     Contando o como começou, com que intuito, quais suas maiores causas.
                 </p>  
             </div>
-            <div className={styles['main']}>
+        <div className={styles['main']}>
             <Image src={image1} className={styles['lampada']}/>
             <div className={styles['bottomtext']}>
                 <p className={styles['text-content']}>
@@ -24,10 +24,10 @@ export default function textoBarraQuemSomos() {
                     Texto meramente ilustrativo.
                 </p>
             </div>
-            </div>
+        </div>
         <div className={styles['imagebox']}>
          <Image src={image2} className={styles['ImageBoxContent']}/>                            {/* procurar ambas imagens com resolução melhor */}
-        <Image src={image3} className={styles['ImageBoxContent']}/>
+        <Image src={image3} className={styles['hiddenImage']}/>
         </div>
         </div>
         </>
