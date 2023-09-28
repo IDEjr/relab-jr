e0563732267c61451dc18402df4ba0e76627f151
lucapcf <lucapcf@gmail.com>
2023-09-28 18:56:08 -0300

    .

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -26,7 +26,7 @@ export default function blog({posts, nav}) {
           titulo={post.titulo}
           data={post.data}
           previa={post.previa}
-          autor={post.autor.nome}
+          autor={post.autor}
         />
       ))}
       <Footer />
