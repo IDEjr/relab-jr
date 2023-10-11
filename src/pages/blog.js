9179586c6ba6bd4fa24b6eeea98f21f1f0f1e247
Nickolas Xisto Machado <cabocanick@gmail.com>
2023-10-11 10:54:08 -0300

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,11 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from '../styles/blog.module.css'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 import styles from '../styles/blog.module.css'
 import MenuBlog from '../components/menuBlog/menuBlog'
 import CardPosts from '../components/cardPosts'
-import GridPosts from '@/components/gridPosts'
+import GridPosts from '../components/gridPosts'
 import { handleJSONfiles } from '../utils/functions/jsonHandler'
 import { handleJSONfile } from '../utils/functions/jsonHandler'
 
@@ -26,7 +25,7 @@ export default function blog({posts, nav}) {
       
       <Navbar />  
       <div className={styles.container}>
-      <MenuBlog posts={posts}/>
+      {/* <MenuBlog posts={posts}/> */}
   
       <Navbar  {...navData}/>
       <GridPosts {...posts}/>
