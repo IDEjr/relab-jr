e54f8f5af71e7e4bf80e366d9727dfb88c363454
tomaee <tomastrindade@gmail.com>
2023-10-13 01:16:03 -0300

    initial work

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -3,14 +3,48 @@ import ReactMarkdown from 'react-markdown'
 import matter from 'gray-matter'
 import styles from './slug.module.css'
 import { handleJSONfile } from '@/utils/functions/jsonHandler'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import pencilImg from '../../../public/uploads/slug/pencil.png'
+import buttonImg from '../../../public/uploads/slug//voltarButton.png'
+
+
+
+const handleMove = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" }); // here it goes
+};
+
+
 
 export default function Posts({ content }) {
+  const router = useRouter();
+  const { postId } = router.query
+
+
   return (
     <div className={styles['container']}>
-      <div className={styles['examples-container']}>
+      <div className={styles['top-part']}>
         <img className={styles['img']} src={`${content.imagem}`}></img>
-        <div>{content.texto}</div>
-        <div className={styles['markdown']}></div>
+      </div>
+      <div className={styles['bottom-part']}>
+        <div className={styles['text-and-rest']}>
+          <div className={styles['text-formater']}>
+            <text className={styles['post-text']}>
+              {content.conteudo}
+            </text>
+          </div>
+          <div className={styles['author']}>
+            <div className={styles['author-and-pencil']}>
+              <Image className={styles['']} src={pencilImg}/>
+              <text className={styles['author-name']}>
+                {content.autor}
+              </text>
+            </div>
+            <button onClick={handleMove} className={styles['voltar-button']}>
+              <Image className={styles['button-image']} src={buttonImg}/>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
