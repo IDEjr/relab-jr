1b88c3b3374cf2c6f134f87696e7a6cbd4a0c6b4
lucapcf <lucapcf@gmail.com>
2023-10-22 16:36:55 -0300

    correção da extração das imagens no footer

--- src/pages/quemSomos.js
+++ src/pages/quemSomos.js
@@ -12,7 +12,7 @@ import { handleJSONfile } from '@/utils/functions/jsonHandler'
 import GridMembros from '@/components/gridMembros'
 import CarrosselQuemSomos from '@/components/carrosselQuemSomos'
 
-export default function quemSomos({quemSomos, membros, nav, carrosselQuemSomos}) {
+export default function quemSomos({quemSomos, membros, nav, carrosselQuemSomos, foo}) {
 console.log(carrosselQuemSomos[0].titulo);
   const path = quemSomos.inicioQuemSomos.imagem;
   const grid = {
@@ -42,7 +42,7 @@ console.log(carrosselQuemSomos[0].titulo);
       <Valores />
       <GridMembros   titulo={grid.titulo} logo={grid.logo} membros={membros}/>
       
-      <Footer />
+      <Footer {...foo}/>
     </>
   );
 }
@@ -53,6 +53,11 @@ export async function getStaticProps(){
   const caminho = "paginas";
   const pagina = "quemSomos";
   const caminho2 = "navFooter";
+
+
+  const pagina3 = "footer";
+  const foo = handleJSONfile(`./content/${caminho2}/${pagina3}.json`);
+
   const pagina2 = "navbar";
   const quemSomos = handleJSONfile(`./content/${caminho}/${pagina}.json`);
   const carrosselQuemSomos = handleJSONfiles(`./content/carrosselQuemSomos`)
@@ -60,6 +65,6 @@ export async function getStaticProps(){
   const membros = handleJSONfiles(`./content/${pasta}`);
   const nav = handleJSONfile(`./content/${caminho2}/${pagina2}.json`);
   return {
-    props: { quemSomos, membros, nav, carrosselQuemSomos },
+    props: { quemSomos, membros, nav, carrosselQuemSomos, foo },
   };
 }
