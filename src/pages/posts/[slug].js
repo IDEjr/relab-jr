438e35e93fc1c2b64a496a8d0ebff60001fd1eb2
tomaee <tomastrindade@gmail.com>
2023-11-06 20:03:19 -0300

    add nav and footer

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -7,6 +7,9 @@ import { useRouter } from 'next/router'
 import Image from 'next/image'
 import pencilImg from '../../../public/uploads/slug/pencil.png'
 import buttonImg from '../../../public/uploads/slug//voltarButton.png'
+import Navbar from '../../components/navbar'
+import Footer from '../../components/footer'
+import React from 'react'
 
 
 
@@ -17,13 +20,22 @@ const handleMove = () => {
 
 
 
-export default function Posts({ content }) {
+export default function Posts({ content, nav, foo }) {
   const router = useRouter();
   const { postId } = router.query
 
+  const navData = {
+    logo : nav.logo,
+    linkedin : nav.linkedin,
+    instagram : nav.instagram,
+    email : nav.email
+  };
 
   return (
     <div className={styles['container']}>
+      <React.Fragment>
+      <Navbar  {...navData}/>
+      </React.Fragment>
       <div className={styles['top-part']}>
         <Image
             src={content.imagemCapa}
@@ -69,16 +81,22 @@ export default function Posts({ content }) {
           </div>
         </div>
       </div>
+      <Footer {...foo}/>
     </div>
   )
 }
 
 export async function getStaticProps({params : {slug} }){
   const caminho = 'posts';
+  const caminho2 = "navFooter";
+  const pagina2 = "navbar";
+  const pagina3 = "footer";
 
   const content = handleJSONfile(`./content/${caminho}/${slug}.json`);
+  const nav = handleJSONfile(`./content/${caminho2}/${pagina2}.json`);
+  const foo = handleJSONfile(`./content/${caminho2}/${pagina3}.json`);
   return {
-    props: { content },
+    props: { content , nav, foo},
   };
 }
 
