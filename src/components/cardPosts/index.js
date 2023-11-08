9c4009b1ea32996c86883aab0660b55c34472c4f
lucapcf <lucapcf@gmail.com>
2023-11-08 17:39:57 -0300

    arrumando cardmembros para acesar slug

--- src/components/cardPosts/index.js
+++ src/components/cardPosts/index.js
@@ -4,32 +4,17 @@ import styles from './cardPosts.module.css'
 import fs from 'fs';
 import path from 'path';
 
-export default function cardPosts({ index, imagem, titulo, data, previa, autor}) {
+export default function cardPosts({ fileName, imagem, titulo, data, previa, autor}) {
 
   var dataForm;
   dataForm = data.substring(8, 10) + '/' + data.substring(5, 7) + '/' + data.substring(0, 4);
 
-
-  index={i}
-
-
-
-  const fs = require('fs');
-  const path = require('path')
-
-  // content\posts\11111111111111111111111111.json
+  const url = `/posts/${fileName}`;
   
-  
-  const directoryPath = './././content/posts';
-
-  const jsonFilenames = fs.readdirSync(directoryPath)
-  .filter((filename) => path.extname(filename) === '.json');
-
-  console.log(jsonFilenames);
 
   return (
     <>
-      <Link href="/" className={styles.link}>
+      <Link href={url} className={styles.link}>
         <div className={styles.card}>
           <div className={styles.box}>
             <div className={styles.img}>
@@ -57,54 +42,3 @@ export default function cardPosts({ index, imagem, titulo, data, previa, autor})
     </>
   );
 }
-
-
-
-
-// export async function getStaticProps() {
-
-//   // obtendo nomes dos arquivos .json
-//   const caminhoPosts = 'posts';
-//   const jsonNomes = fs.readdirSync(`./content/${caminhoPosts}`)
-//     .filter(fileName => path.extname(fileName) === '.json');
-
-//   return {
-//     props: {jsonNomes },
-//   };
-// }
-
-
-
-
-
-// const jsonsInDir = fs.readdirSync('content/posts').filter(file => path.extname(file) === '.json');
-  
-//   jsonsInDir.forEach(file => {
-//     const fileData = fs.readFileSync(path.join('content/posts', file));
-//     const json = JSON.parse(fileData.toString());
-//   });
-
-
-
-
-
-// function fetchJSONFilenames() {
-//   const caminhoPosts = 'posts';
-//   const jsonNomes = fs.readdirSync(join(process.cwd(), `content/${caminhoPosts}`))
-//     .filter(fileName => path.extname(fileName) === '.json');
-//   return jsonNomes;
-// }
-
-// Fetch JSON filenames
-// const jsonNomes = fetchJSONFilenames();
-
-
-
-// console.log(index);
-// console.log('jsonNomes:', jsonNomes); 
-
-
-
-// url = `/posts/${jsonNomes[index]}`;
-
-// {url}
