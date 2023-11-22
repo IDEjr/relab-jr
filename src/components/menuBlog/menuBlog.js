c22284389ae868988d84ddd10740bbf56082e8f9
Nickolas Xisto Machado <106843878+Nickolas161202@users.noreply.github.com>
2023-11-22 15:22:42 -0300

    dropdown customizado criado

--- src/components/menuBlog/menuBlog.js
+++ src/components/menuBlog/menuBlog.js
@@ -6,71 +6,82 @@ import GridPosts from '../gridPosts'
 
 
 
-export default function MenuBlog( ...posts) {
-    const handleChange = (event) =>{
-        filter(`${event.target.value}`, arrPosts); //a função pega o valor passado pelo campo "value" de cada opção e guarda no hook
-    } 
+export default function MenuBlog(...posts) {
+    const [isActive, setIsactive] = useState(false)
+    const [titulo, setTitulo] = useState("TODOS")
+
     posts = posts[0]
     var genderSelection = []
-   
-  
     var arrPosts = [];
-    for( const i in posts){
 
+    for (const i in posts) {
         arrPosts.push(posts[i]);
     }
-    const [filteredPosts, setFilteredPosts] = useState(arrPosts)
-arrPosts.map((arrPosts) =>{genderSelection.push(arrPosts.genero)});
-const filteredgenders = [...new Set(genderSelection)];
 
-const filter  = (select, arrPosts) =>{
-    let aux  =  arrPosts;
-    (select == "TODOS")? aux =  arrPosts: 
-     aux = arrPosts.filter((obj) =>obj.genero == select )
-    setFilteredPosts(aux)
-}
+    const [filteredPosts, setFilteredPosts] = useState(arrPosts)
+
+    arrPosts.map((arrPosts) => { genderSelection.push(arrPosts.genero) });
+    const filteredgenders = [...new Set(genderSelection)];
+
+    const filter = (select, arrPosts) => {
+        let aux = arrPosts;
+        (select == "TODOS") ? aux = arrPosts :
+            aux = arrPosts.filter((obj) => obj.genero == select)
+        setFilteredPosts(aux)
+        setTitulo(select)
+        setIsactive(false)
+    }
 
 
     return (
         <>
-        <div className={Styles.container}>
-            <div className={Styles.menuContainer}>
-                <ul className={Styles.lista}>
-                    <li key={'mobile'}>
-                        <div >
-                            <select className={Styles.buttonMobile} onChange={handleChange}>
-                            <option value={"TODOS"}> Todos</option>
-
-                            {filteredgenders.map((filteredgenders) => ( <option> 
-                            {filteredgenders}
-                            </option>
-                        ))}
-
-                            </select>
-
+            <div className={Styles.container}>
+                <div className={Styles.menuContainer}>
+                    <ul className={Styles.lista}>
+                        <div>
+                            <div className={Styles.containerMobile}>
+                                <h3 className ={Styles.tituloMobile} ><p>{titulo}</p></h3>
+                                <h4  className ={Styles.mais}onClick={() => setIsactive(!isActive) }>Outros</h4>
+                            </div>
+                            
+                            
+                            
+                            {isActive && (
+                                <div className={Styles.optionsMobile}>
+                                       <li> <button className={Styles.buttonMobile} onClick={() => filter("TODOS", arrPosts)}> Todos </button> </li>
+                                    {filteredgenders.map((filteredgenders) => (<li key={filteredgenders.titulo}>
+                                        <button className={Styles.buttonMobile} onClick={() => { filter(filteredgenders, arrPosts) }}>
+                                            {filteredgenders}
+                                        </button>
+                                    </li>
+                                    ))}
+                                </div>
+                            )}
+                        
                         </div>
-                    </li>
-                        <li key={'todos'}>
-                            <button  className={Styles.button} onClick={() => filter("TODOS", arrPosts)}> Todos </button>
+
+
+                        <li key={'todos'} className={Styles.optionDesktop}>
+                            <button className={Styles.button} onClick={() => filter("TODOS", arrPosts)}> Todos </button>
                         </li>
-
-                        {filteredgenders.map((filteredgenders) => ( <li key={filteredgenders.titulo}>
-                            <button className={Styles.button} onClick={() => {filter(filteredgenders, arrPosts) }}> 
-                            {filteredgenders}
+                        
+                        {filteredgenders.map((filteredgenders) => (<li key={filteredgenders.titulo} className={Styles.optionDesktop}>
+                            <button className={Styles.button} onClick={() => { filter(filteredgenders, arrPosts) }}>
+                                {filteredgenders}
                             </button>
-                            </li>
+                        </li>
                         ))}
-                </ul>
+                    </ul>
+                </div>
+
+
+                <GridPosts {...filteredPosts} />
+
+
+
+
+
             </div>
-
-            
-            <GridPosts {...filteredPosts}/>
-            
-            
-         
-
-         
-         </div>                           
-        </> 
+        </>
     )
 }
