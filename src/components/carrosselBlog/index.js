4c8571778409e6c7413a6bab3e38fee7e59457cf
Nickolas Xisto Machado <106843878+Nickolas161202@users.noreply.github.com>
2023-12-21 15:34:03 -0300

    resize do dropdown mobile
    aumentado o tamanho geral do dropdown, dos botões, adicionado o ícone.

--- src/components/carrosselBlog/index.js
+++ src/components/carrosselBlog/index.js
@@ -71,13 +71,12 @@ export default function CarrosselBlog(...posts) {
             slidesPerView={1}
             effect={'swipe'}
             pagination={{ clickable: next }} //permite a troca de páginas pelo pagination
-            navigation={{  enabled: false  }}
+            navigation={{  enabled: false,   }}
             slidesPerGroup={1}
             autoplay={{  delay: 5000, disableOnInteraction: false  }}
             className={styles.swiperContainer}
             breakpoints={{ 800: {  navigation: { enabled: true }  }}}  //se a tela for maior que 800 pixels, ativa o navigation(as setinhas)
           > 
-          {(console.log(recents))}
             {recents.map((item) =>  ( //renderiza um slide para cada item no array de recents
               
               <SwiperSlide key={item.titulo} className={styles.swiperInd}>
