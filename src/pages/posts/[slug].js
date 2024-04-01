483e6f9560e55f17c2094acd3bfd2682f0c17b39
lucapcf <lucapcf@gmail.com>
2024-04-01 00:06:24 -0300

    made some changes to send to relabjr, might be reverted after

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -33,74 +33,68 @@ export default function Posts({ content, nav, foo }) {
     dataForm = content.data.substring(8, 10) + '/' + content.data.substring(5, 7) + '/' + content.data.substring(0, 4);
 
   return (
-    <div className={styles.container}>
-      <React.Fragment>
-        <Navbar  {...navData} />
-      </React.Fragment>
-      <div className={styles.topPart}>
-        <Image
-          src={content.imagemCapa}
-          fill={true}
-          className={styles.topImg}
-          style={{objectFit: 'cover', background: 'black'}}
-        />
-        <p class="newStyle"></p>
-        <div className={styles.contentBlock}>
-          <div className={styles.titleAndRest}>
-            <text className={styles.categorie}>
-              {content.genero}
-            </text>
-            <h1 className={styles.title}>
-              {content.titulo}
-            </h1>
-            <text className={styles.subTitle}>
-              {content.previa}
-            </text>
-          </div>
-
-
-          <text className={styles.details}>
-            <a href={content.linkedin} className={styles['autor']}>
-              {content.autor}
-            </a>
-            <span />
-            {dataForm}
-          </text>
-        </div>
-      </div>
-      <div className={styles.bottomPart}>
-        <div className={styles.textAndRest}>
-          <div className={styles.textFormater}>
-
-
-
-            <ReactMarkdown className={styles.markdown}
-              components={{ img: ({ node, ...props }) => <img style={{ maxWidth: '100%', display: 'block', padding: '5vh 0px 5vh 0px', margin: 'auto' }}{...props} /> }}>
-              {content.previa}
-            </ReactMarkdown>
-            <ReactMarkdown className={styles.markdown2}>
-              {content.conteudo}
-            </ReactMarkdown>
-
-
-          </div>
-          <div className={styles.author}>
-            <div className={styles.authorAndPencil}>
-              <Image src={pencilImg} />
-              <text className={styles.authorName}>
-                <a href={content.linkedin} className={styles['autor']}>
-                  {content.autor}
-                </a>
-              </text>
+    <>
+      <div className={styles.container}>
+        <React.Fragment>
+          <Navbar {...navData} />
+        </React.Fragment>
+        <div className={styles.topPart}>
+          <Image
+            src={content.imagemCapa}
+            fill
+            className={styles.topImg}
+            style={{objectFit: 'cover', background: 'black'}}
+          />
+          <p class="newStyle"></p>
+          <div className={styles.contentBlock}>
+            <div className={styles.titleAndRest}>
+              <p className={styles.categorie}>
+                {content.genero}
+              </p>
+              <h1 className={styles.title}>
+                {content.titulo}
+              </h1>
+              <p className={styles.subTitle}>
+                {content.previa}
+              </p>
             </div>
+            <div className={styles.details}>
+              <a href={content.linkedin} target="_blank" className={styles['autor']}>
+                {content.autor}
+              </a>
+              <p className={styles.data}>
+                {dataForm}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.markDown}>
+          {/* <ReactMarkdown className={styles.markdown}
+            components={{ img: ({ node, ...props }) => <img style={{ maxWidth: '100%', display: 'block', padding: '5vh 0px 5vh 0px', margin: 'auto' }}{...props} /> }}>
+            {content.previa}
+          </ReactMarkdown> */}
+          <ReactMarkdown className={styles.markdown}>
+            {content.conteudo}
+          </ReactMarkdown>
+        </div>
+        <div className={styles.bottomContainer}>
+          <div className={styles.authorAndPencil}>
+            <Image src={pencilImg} />
+            <text className={styles.authorName}>
+              <a href={content.linkedin} className={styles['autor']}>
+                {content.autor}
+              </a>
+            </text>
+          </div>
+          <div className={styles.buttonContainer}>
             <button onClick={handleMove} className={styles.voltarButton}>
-              <Image className={styles.buttonImage} src={buttonImg} />
+              Voltar
             </button>
           </div>
         </div>
+        <Footer {...foo} />
       </div>
-      <Footer {...foo} />
-    </div>
+    </>
   )
 }
 
