7f82193aeb27e0c0678d55f3d86e0bb8fdb64ac2
lucapcf <lucapcf@gmail.com>
2024-05-17 17:43:50 -0300

    Making Blog and postPage more responsive

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -1,13 +1,13 @@
+import styles from './slug.module.css'
+import Image from 'next/image'
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
 import fs from 'fs'
-import ReactMarkdown from 'react-markdown'
-import styles from './slug.module.css'
-import Image from 'next/image'
 import { handleJSONfile } from '@/utils/functions/jsonHandler'
 import { useRouter } from 'next/router'
-import React from 'react'
 import { RxPencil1 } from "react-icons/rx"
+import ReactMarkdown from 'react-markdown'
+import React from 'react'
 
 
 const handleMove = () => {
@@ -47,16 +47,10 @@ export default function Posts({ post, nav, foo, blog }) {
           />
           <div className="newStyle"></div>
           <div className={styles.postBlock}>
-            <div className={styles.titleAndRest}>
-              <p className={styles.categorie}>
-                {post.genero}
-              </p>
-              <h1 className={styles.title}>
-                {post.titulo}
-              </h1>
-              <p className={styles.subTitle}>
-                {post.previa}
-              </p>
+            <div className={styles.text}>
+              <h3 className={styles.genero}>{post.genero}</h3>
+              <h2 className={styles.title}>{post.titulo}</h2>
+              <h3 className={styles.subTitle}>{post.previa}</h3>
             </div>
             <div className={styles.details}>
               <a href={post.linkedin} target="_blank" className={styles.autor}>
