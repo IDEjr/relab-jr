3b1ba98bdb866931f7fe9e46c87c65dfae386644
lucapcf <lucapcf@gmail.com>
2024-05-21 02:11:17 -0300

    FIxing textoQuemsomos resposiveness

--- src/components/textoBarraQuemSomos/index.js
+++ src/components/textoBarraQuemSomos/index.js
@@ -6,20 +6,20 @@ export default function TextoBarraQuemSomos({ texto1, texto2, logo }) {
   return (
     <>  
       <div className={styles.container}> {/* inicio da div container */}
-        <div className={styles.toptext}>
+        <div className={styles.topText}>
           <p className={styles.textContent}>{texto1}</p>
         </div>
-        <div className={styles.retangulo}></div>
+        <div className={styles.bar}></div>
         <div className={styles.textImage}> {/* inicio da div textImage */}
           <div className={styles.lampada}>
             <Image
               src={logo}
               fill
               style={{ objectFit: 'cover' }}
-              alt="Post"
+              alt="Logo"
             />
           </div>
-          <div className={styles.bottomtext}>
+          <div className={styles.bottomText}>
             <p className={styles.textContent}>{texto2}</p>
           </div>
         </div> {/* fim da div textImage */}
