e2a48829e5420df9d44bf1035b59016dedf03632
lucapcf <119978877+lucapcf@users.noreply.github.com>
2024-06-19 17:37:15 -0300

    Revert "testing value of environment variable (email)"
    This reverts commit 75ada08fb7c0113421f4119d1b1bb905370d33ad.

--- src/utils/functions/nodemailer.js
+++ src/utils/functions/nodemailer.js
@@ -4,8 +4,6 @@ import nodemaliler from "nodemailer"
 const email = process.env.EMAIL_FORM;
 const password = process.env.SENHA_DE_APP;
 
-console.log(`EMAIL_FORM: ${email}`)
-
 export const transporter = nodemaliler.createTransport({
     service: 'gmail',
     auth:{
