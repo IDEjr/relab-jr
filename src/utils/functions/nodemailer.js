75ada08fb7c0113421f4119d1b1bb905370d33ad
lucapcf <119978877+lucapcf@users.noreply.github.com>
2024-06-19 17:28:27 -0300

    testing value of environment variable (email)

--- src/utils/functions/nodemailer.js
+++ src/utils/functions/nodemailer.js
@@ -4,6 +4,8 @@ import nodemaliler from "nodemailer"
 const email = process.env.EMAIL_FORM;
 const password = process.env.SENHA_DE_APP;
 
+console.log(`EMAIL_FORM: ${email}`)
+
 export const transporter = nodemaliler.createTransport({
     service: 'gmail',
     auth:{
